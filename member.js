9b1217ad23c3010d2ad8d5127e64860de150a73e
Sarthak8874 <108093297+Sarthak8874@users.noreply.github.com>
2024-01-25 15:55:59 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,11 @@
 function skillsMember(){
-    
+    return{
+        restrict:'E',
+        templateUrl:'modules/skills/views/member.html',
+        controllerAs:'vm',
+        blindToController:true,
+        scope:{
+            member:'='
+        }
+    }
 }
